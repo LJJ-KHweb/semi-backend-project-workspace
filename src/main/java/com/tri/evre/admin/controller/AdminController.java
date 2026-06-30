8fa6d56ad3c964fd3b95aef5e91cdb970486315c
LJJ_KHweb <blue7257@gamil.com>
2026-06-30 15:18:14 +0900

    관지라일반게시판전체조회

---
+++ src/main/java/com/tri/evre/admin/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.tri.evre.admin.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tri.evre.admin.model.service.AdminService;
+import com.tri.evre.board.model.dto.BoardListResponse;
+import com.tri.evre.common.model.dto.PageInfo;
+import com.tri.evre.global.api.model.vo.ApiResponse;
+import com.tri.evre.global.api.model.vo.CustomHttpStatus;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+	private final AdminService adminService;
+
+	@GetMapping("/boards")
+	public ResponseEntity<ApiResponse<BoardListResponse>> findAll(@RequestParam(name = "page", defaultValue = "0") int page,
+												@RequestParam(name = "size", defaultValue = "3") int size) {
+		BoardListResponse boardListResponse = adminService.findAll(new PageInfo(page, size));
+		return ResponseEntity.status(CustomHttpStatus.SELECT_SUCCESS.getCode()).body(ApiResponse.success("(관리자)게시글 조회 성공", boardListResponse));
+	}
+}
