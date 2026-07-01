297263f6c71319d00eab5206f0d21c4a35ae0f6a
kkimmun <ksk01099645771@gmail.com>
2026-07-01 12:29:46 +0900

    상품 구매 내역 조회

--- src/main/java/com/tri/evre/shop/controller/ShopController.java
+++ src/main/java/com/tri/evre/shop/controller/ShopController.java
@@ -1,4 +1,6 @@
-package com.tri.evre.shop.model.controller;
+package com.tri.evre.shop.controller;
+
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tri.evre.global.api.model.vo.ApiResponse;
 import com.tri.evre.global.api.model.vo.CustomHttpStatus;
 import com.tri.evre.global.auth.model.vo.CustomUserDetails;
-import com.tri.evre.shop.model.dto.ProductDto;
+import com.tri.evre.shop.model.dto.HistoryPurchaseDto;
+import com.tri.evre.shop.model.dto.HistoryPurchaseListDto;
 import com.tri.evre.shop.model.dto.ProductListResponse;
 import com.tri.evre.shop.model.service.ShopService;
 
@@ -33,15 +36,31 @@ public class ShopController {
 		ProductListResponse productResponse = shopService.findAll(page, size);
 		return ResponseEntity.status(CustomHttpStatus.SELECT_SUCCESS.getCode()).body(ApiResponse.success("조회성공", productResponse));
 	}
-	
-	@PatchMapping("{productNo}")
-	public ResponseEntity<ApiResponse<Void>> purchase(@PathVariable(name="productNo") Long productNo,
+		
+	@PatchMapping("/{productNo}")
+	public ResponseEntity<ApiResponse<Void>> purchase(@PathVariable(value = "productNo") Long productNo,
 													  @AuthenticationPrincipal CustomUserDetails user){
 		
+		log.info("{}", productNo);
 		shopService.purchase(productNo, user);
 		
 		return ResponseEntity.status(CustomHttpStatus.UPDATE_SUCCESS.getCode()).body(ApiResponse.success("구매 성공", null));
 	}
+	
+	// ------------------------07/01--------------------------
+	
+	@GetMapping("/his-products")
+	public ResponseEntity<ApiResponse<HistoryPurchaseListDto>> findByHistoryPurchase(@RequestParam("page") int page
+																					  ,@RequestParam("size") int size
+																					  ,@AuthenticationPrincipal CustomUserDetails user){
+		
+		HistoryPurchaseListDto response = shopService.findByHistoryPurchase(page, size, user);
+		
+		return ResponseEntity.status(CustomHttpStatus.SELECT_SUCCESS.getCode()).body(ApiResponse.success("조회 성공", response));
+	}
+	
+	
+	
 
 	
 }
