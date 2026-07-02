7a82b61baee3adab611c43926d17f7ea69f1e346
LJJ_KHweb <blue7257@gamil.com>
2026-07-02 17:08:49 +0900

    feat: 마이페이지 최종 수정본

--- src/main/java/com/tri/evre/rasp/model/service/RaspService.java
+++ src/main/java/com/tri/evre/rasp/model/service/RaspService.java
@@ -1,15 +1,16 @@
 package com.tri.evre.rasp.model.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.tri.evre.common.model.dto.PageInfo;
 import com.tri.evre.global.auth.model.vo.CustomUserDetails;
 import com.tri.evre.global.exception.rasp.RaspNotFoundException;
 import com.tri.evre.rasp.model.dao.RaspMapper;
 import com.tri.evre.rasp.model.dto.RaspDayOfWeek;
 import com.tri.evre.rasp.model.dto.RaspDto;
+import com.tri.evre.rasp.model.dto.RaspResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,19 +24,27 @@ public class RaspService {
 	private final RaspMapper raspMapper;
 
 	// 탄소절감량 계산 공식
-	private void calculateCarbonReduction(List<RaspDayOfWeek> results) {
+	private List<RaspResponseDto> calculateCarbonReduction(List<RaspDayOfWeek> results) {
+		List<RaspResponseDto> responseResults = new ArrayList();
 		for (RaspDayOfWeek r : results) {
 			// 탄소 감축량 = 거리 * (내연기간 탄소배출량 - 전력생산 * 전비(전체거리 / 소비 전력량));
 			double efficiency = r.getDistanceSum() / r.getKilowattSum();
 			r.setCarbonReduction(r.getDistanceSum() * (ICE_CO2_EMISSION - (ELECTRICITY_EMISSION_FACTOR / efficiency)));
+			responseResults.add(RaspResponseDto.builder().dayDate(r.getDayDate()+ "(" + r.getDay() +")")
+														.distanceSum(r.getDistanceSum())
+														.carbonReduction(r.getCarbonReduction())
+														.build());
+			
 		}
+		return responseResults;
+		
 	}
 
 	public void save(RaspDto rasp) {
 		int reuslt = raspMapper.save(rasp);
 	}
 
-	public List<RaspDayOfWeek> findAll() {
+	public List<RaspResponseDto> findAll() {
 
 		List<RaspDayOfWeek> results = raspMapper.findAll();
 		if (results.isEmpty()) {
@@ -45,18 +54,18 @@ public class RaspService {
 			throw new RaspNotFoundException("라즈베리디비 요일별 조회 실패했습니다.");
 		}
 
-		calculateCarbonReduction(results);
+		List<RaspResponseDto> responseResults = calculateCarbonReduction(results);
 
-		return results;
+		return responseResults;
 	}
 
-	public List<RaspDayOfWeek> findMyRaspAll(CustomUserDetails user) {
+	public List<RaspResponseDto> findMyRaspAll(CustomUserDetails user) {
 		List<RaspDayOfWeek> results = raspMapper.findMyRaspAll(user.getUsername());
 		if (results.isEmpty()) {
 			throw new RaspNotFoundException("라즈베리디비 조회 실패했습니다.");
 		}
-		calculateCarbonReduction(results);
-		return results;
+		List<RaspResponseDto> responseResults = calculateCarbonReduction(results);
+		return responseResults;
 	}
 
 }
