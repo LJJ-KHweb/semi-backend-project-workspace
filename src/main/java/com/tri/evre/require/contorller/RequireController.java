55359ecc90e7dfea48edd9487b62494d828fe113
kkimmun <ksk01099645771@gmail.com>
2026-07-06 18:43:44 +0900

    다시 만들기

--- src/main/java/com/tri/evre/require/contorller/RequireController.java
+++ src/main/java/com/tri/evre/require/contorller/RequireController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,14 +37,22 @@ public class RequireController {
 	public ResponseEntity<ApiResponse<?>> writeRequire(@ModelAttribute @Valid RequireDto require,
 												  	   @RequestParam(name="file", required =false) List<MultipartFile> files,
 												  	   @AuthenticationPrincipal CustomUserDetails user){
-		
 		requireService.writeRequire(require, files, user);
 		
-		
-		
-		
+
 		return ResponseEntity.status(CustomHttpStatus.SELECT_SUCCESS.getCode()).body(ApiResponse.success("문의사항 작성 성공", null));
 	}
+	
+	@GetMapping
+	public ResponseEntity<ApiResponse<?>> findAll(@RequestParam("page") int page,
+												  @RequestParam("size") int size,
+												  @AuthenticationPrincipal CustomUserDetails user){
+
+		
+
+
+return ResponseEntity.status(CustomHttpStatus.SELECT_SUCCESS.getCode()).body(ApiResponse.success("문의사항 작성 성공", null));
+}
 	
 	
 }
